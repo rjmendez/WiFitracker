87f857be5fac304cb2860b047137e72310ee7c85
rjmendez <robertjmendez@gmail.com>
2016-11-21 19:46:21 -0600

    Changing path to an example user.

--- exporttocsv.sh
+++ exporttocsv.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 STOR_SRV=storage.example.com
+STOR_PATH=/home/user/path
 DATE=`date +%Y-%m-%d-%H.%M.%S`
 echo "Dumping blue_hydra.db to blue_hydra.csv"
 sqlite3 -header -csv /opt/pwnix/data/blue_hydra/blue_hydra.db "select * from blue_hydra_devices;" > /var/log/pwnix/blue_hydra-$DATE.csv
 cp /opt/pwnix/data/blue_hydra/blue_hydra.db /var/log/pwnix/blue_hydra-$DATE.db
-rsync -avz /var/log/pwnix/blue_hydra* $STOR_SRV:/home/rjmendez/blue_hydra
+rsync -avz /var/log/pwnix/blue_hydra* $STOR_SRV:$STOR_PATH
 echo "Done!"
