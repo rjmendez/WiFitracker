f0ce5c35e2fd8fd222d3adcaef81fd42d4bdd817
rjmendez <robertjmendez@gmail.com>
2016-11-21 19:44:32 -0600

    Removing references to floppydicks.net :D

--- exporttocsv.sh
+++ exporttocsv.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+STOR_SRV=storage.example.com
 DATE=`date +%Y-%m-%d-%H.%M.%S`
 echo "Dumping blue_hydra.db to blue_hydra.csv"
 sqlite3 -header -csv /opt/pwnix/data/blue_hydra/blue_hydra.db "select * from blue_hydra_devices;" > /var/log/pwnix/blue_hydra-$DATE.csv
 cp /opt/pwnix/data/blue_hydra/blue_hydra.db /var/log/pwnix/blue_hydra-$DATE.db
-rsync -avz /var/log/pwnix/blue_hydra* enzo.floppydicks.net:/home/rjmendez/blue_hydra
+rsync -avz /var/log/pwnix/blue_hydra* $STOR_SRV:/home/rjmendez/blue_hydra
 echo "Done!"
